ddccfc04b1bfba9b2ba5c56eb70fe46e35a90f06
duy.pham <duy.pham@logixtek.com>
2020-11-10 10:17:01 +0700

    Add Error and WeatherReport - report.jsx: Start building Weather Report - error.jsx: provide a means to display errors - index.jsx: render Error and Weather Report

--- weather-app/src/components/weather/index.jsx
+++ weather-app/src/components/weather/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react';
+// Local Components
+import ErrorReport from './error';
+import WeatherReport from './report';
 // Engine
 import { getData, myLocation } from '../../engine';
-import { objectHasProperty } from '../../engine/tools';
+import { objectHasProperty, isObjectEmpty } from '../../engine/tools';
 class Weather extends React.Component {
     constructor(props) {
         super(props);
@@ -24,11 +28,15 @@ class Weather extends React.Component {
             });
     }
     render() {
-        const obj = this.state;
-        console.log('Data Object', obj); return (
-            <h1>
-                Hello World!
-            </h1>
+        const obj = this.state; const isEmpty = isObjectEmpty(obj);
+        const isError = objectHasProperty(obj, 'error');
+        return (
+            <div>
+                <Dimmer active={isEmpty} inverted>
+                    <Loader inverted />
+                </Dimmer>
+                {isError ? <ErrorReport {...obj} /> : <WeatherReport {...obj} />}
+            </div>
         );
     }
 }
